89ce82410011f7a0c3785ae8629f83c544c2a621
HiepHV <han.van.hiep@framgia.com>
2015-10-19 14:46:31 +0700

    add joinad.sh

---
+++ joinad.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+echo -e "nameserver 192.168.0.2 \nsearch framgia.org" > /etc/resolv.conf
+dpkg -i likewise-open*.deb
+echo -e '%FRAMGIA\\\domain^users ALL=(ALL) ALL' >> /etc/sudoers
+echo -e "[SeatDefaults] \ngreeter-show-manual-login=true" >> /etc/lightdm/lightdm.conf
+echo "emFxMTJ3c3hAMTIzCg==" | openssl enc -base64 -d | domainjoin-cli join framgia.com Administrator
